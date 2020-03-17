737ed2aa3611e8e77ddad16bb57afece8ad092e9
benjawmin <62154409+benjawmin@users.noreply.github.com>
2020-03-17 16:58:11 -0700

    Add files via upload

---
+++ hw06.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+/*
+struct BookList
+{
+    int r;
+    BookEntry *head;
+    BookEntry *tail;
+};
+
+struct BookEntry;
+{
+    BookList *lst;
+    BookEntry *next;
+    BookEntry *pre;
+    BookData *data;
+};
+
+struct BookData
+{
+    string isdn;
+    string author;
+    string title;
+};
+*/
