7880fb82e17dfb5711637ac471469e96e41477ca
benjawmin <62154409+benjawmin@users.noreply.github.com>
2020-03-24 11:42:03 -0700

    Update hw06.h

--- hw06.h
+++ hw06.h
@@ -1,30 +1,36 @@
 #pragma once
 
-#include <iostream>
-#include <fstream>
+#include<iostream>
+#include<fstream>
 
 using namespace std;
 
-/*
+typedef struct BookList BookList;
+typedef struct BookEntry BookEntry;
+typedef struct BookData BookData;
+
 struct BookList
 {
     int r;
-    BookEntry *head;
-    BookEntry *tail;
+    BookEntry* head;
+    BookEntry* tail;
 };
 
-struct BookEntry;
+struct BookEntry
 {
-    BookList *lst;
-    BookEntry *next;
-    BookEntry *pre;
-    BookData *data;
+    BookList* lst;
+    BookEntry* next;
+    BookEntry* pre;
+    BookData* data;
 };
 
 struct BookData
 {
-    string isdn;
+    string isbn;
     string author;
     string title;
 };
-*/
+//void delBook(BookEntry* loop, BookList* lst, int number);
+void showBooks(BookEntry* loop);
+void showBook(BookEntry* loop, int number);
+void addBook(BookEntry* loop, BookList* lst);
